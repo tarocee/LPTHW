f3a63ccfd1e7118159795c1456717b20928d7886
kainoup <tarocee>
2026-05-04 11:43:30 +0200

    Use shorthand notation +=

--- ex20.py
+++ ex20.py
@@ -25,9 +25,8 @@ print("Let's print three lines:")
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
-
